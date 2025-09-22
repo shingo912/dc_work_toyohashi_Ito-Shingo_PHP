b126bc2f1da8bd58fd16608e4de53936563fde47
shingo912 <shingo9345abcd@gmail.com>
2025-09-22 15:18:30 +0900

    9/22 php

---
+++ htdocs/htdocs/work02/work02.php
@@ -0,0 +1,22 @@
+<?php
+$height = 17; 		//変数$heightを定義し、値を代入
+const WEIGHT = 6;	//定数WEIGHTを定義し、値を代入
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>work02</title>
+</head>
+<body>
+<?php
+    print '身長 ';
+    print $height;
+    print 'cm ';
+    print '体重 '.WEIGHT.'kg';
+?>
+</body>
+</html>
+
+
+
